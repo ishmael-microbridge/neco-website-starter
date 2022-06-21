937abd32863b64d91f87462257776eaa7599f1cd
ishmael-microbridge <ismaila.isiaka@microbridge.com.ng>
2022-06-21 22:09:57 +0100

    chore: add and configure tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      borderWidth: {
+        1: '1px',
+      },
+    },
+  },
+  plugins: [],
+}
